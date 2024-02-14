be65ef84a0a77385080dfb2c296c661ad124296a
Ramisa789 <144377748+Ramisa789@users.noreply.github.com>
2024-02-14 09:04:46 -0700

    Add files via upload

---
+++ ReliefService.java
@@ -0,0 +1,64 @@
+package edu.ucalgary.oop;
+
+public class ReliefService {
+    private Inquirer inquirer;
+    private DisasterVictim missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;
+
+    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        this.dateOfInquiry = dateOfInquiry;
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    // Getters and setters
+    public Inquirer getInquirer() {
+        return inquirer;
+    }
+
+    public void setInquirer(Inquirer inquirer) {
+        this.inquirer = inquirer;
+    }
+
+    public DisasterVictim getMissingPerson() {
+        return missingPerson;
+    }
+
+    public void setMissingPerson(DisasterVictim missingPerson) {
+        this.missingPerson = missingPerson;
+    }
+
+    public String getDateOfInquiry() {
+        return dateOfInquiry;
+    }
+
+    public void setDateOfInquiry(String dateOfInquiry) {
+        // Validate date format
+        if (!dateOfInquiry.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            throw new IllegalArgumentException("Invalid date format");
+        }
+        this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getInfoProvided() {
+        return infoProvided;
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public String getLogDetails() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Inquirer: ").append(inquirer.getFirstName()).append(" ").append(inquirer.getLastName());
+        builder.append(", Missing Person: ").append(missingPerson.getName());
+        builder.append(", Date of Inquiry: ").append(dateOfInquiry);
+        builder.append(", Info Provided: ").append(infoProvided);
+        builder.append(", Last Known Location: ").append(lastKnownLocation.getName());
+        return builder.toString();
+    }
+}
